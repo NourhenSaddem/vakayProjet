6d83dd8a06780d4ce7e4010fc6a74ce739eae031
NourhenSaddem <65470006+NourhenSaddem@users.noreply.github.com>
2021-02-07 02:37:14 +0000

    Update addpanier.php

--- addpanier.php
+++ addpanier.php
@@ -1,14 +1,18 @@
 <?php 
 require 'db.class.php';
 require 'panier.class.php';
-$db = new DB();
+$DB = new DB();
 $panier = new panier();
-if(isset($_Grt['id'])){
-  $prod = $db->query('SELECT id FROM produit WHERE id=:id' , array('id' => $_GET['id']));
-  var_dump($prod);
+if(isset($_GET['id'])){
+  $prod = $DB->query('SELECT id FROM produit WHERE id=:id' , array('id' => $_GET['id']));
+  if(empty($prod)){
+    die("This product doesn't exist");
+  }
+
+$panier->add($prod[0]->id);
+die ('Product added to cart successfully <a href="javascript:history.back()">Return</a>');
 }
 else{
     die("Your cart is empty");
 }
 ?>
-
